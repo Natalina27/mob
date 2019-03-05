ae5c537d4898cdf0f3244e9bd4b386a8ca720bb4
kitaye <44532933+kitaye@users.noreply.github.com>
2019-03-05 13:17:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// function function_afterEmailCheckbox() {
+// 		{
+// 		if (document.getElementById("input2-2").click) 
+// 			{
+// 			document.getElementById("set_time_and_date").style.display = 'block'; 	 
+// 			} 
+// 	};
+// }
