8efb6f9ae9e55f919f08d991a783d3c345f547fe
kitaye <44532933+kitaye@users.noreply.github.com>
2019-03-10 21:06:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// function function_afterEmailCheckbox() {
-// 		{
-// 		if (document.getElementById("input2-2").click) 
-// 			{
-// 			document.getElementById("set_time_and_date").style.display = 'block'; 	 
-// 			} 
-// 	};
-// }
+function ascentText(labelClass) {
+	const input = document.querySelector(labelClass);
+
+	input.addEventListener("blur", function() {
+		if (input.value.length > 0) {
+			input.classList.add("form__input-up");				
+		} else {
+			input.classList.remove("form__input-up");	
+		}
+	}); 
+}
+
+ascentText(".js-ascent-text");
+
+// При событии click на крестике нужно проверить есть ли в инпуте какой-то текст.
+// Если есть, то удаляем текст из инпута.
