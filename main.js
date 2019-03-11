71469b206bb34f6c4b51b8dae280d56eb8db6312
kitaye <44532933+kitaye@users.noreply.github.com>
2019-03-11 20:15:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ ascentText(".js-ascent-text");
 
 // При событии click на крестике нужно проверить есть ли в инпуте какой-то текст.
 // Если есть, то удаляем текст из инпута.
+
+function clearField() {
+	const crossInput = document.querySelector(".js-clear-field");
+	const input = document.querySelector(".js-ascent-text");
+
+	crossInput.addEventListener("click", function() {
+		if (input.value.length > 0) {
+			document.querySelector(".input-close-btn").style.display = "block";
+
+		}
+	})
+}
+
+clearField();
+
+console.log(input);
