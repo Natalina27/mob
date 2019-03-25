61346a3a4bc3a2edf1b19c3e327ae5b270efdb0d
kitaye <44532933+kitaye@users.noreply.github.com>
2019-03-25 21:00:50 +0300

    Delete mobile.js

--- mobile.js
+++
@@ -1,8 +0,0 @@
-// function function_afterEmailCheckbox() {
-// 		{
-// 		if (document.getElementById("input2-2").click) 
-// 			{
-// 			document.getElementById("set_time_and_date").style.display = 'block'; 	 
-// 			} 
-// 	};
-// }
